0c9acf3f232a9bf8f22d1505abcdba44997ccb97
anton holmberg <anton@LAPTOP-SJM6KSDV.localdomain>
2025-07-28 21:27:45 +0200

    moved the nn that was the SCM base model to become an edge transformation function

---
+++ src/tspfn/data/edge_functions/__init__.py
@@ -0,0 +1,3 @@
+from tspfn.data.edge_functions.shallow_nn import nn_edge
+
+__all__ = ["nn_edge"]
---
+++ src/tspfn/data/edge_functions/shallow_nn.py
@@ -0,0 +1,26 @@
+import torch
+from torch.nn.utils import prune
+
+from tspfn.data import PriorHyperParameters
+
+
+class nn_edge(torch.nn.Module):
+    def __init__(self, prior_hp : PriorHyperParameters, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.n_layers = prior_hp.nn_depth
+        self.n_nodes = prior_hp.nn_width
+        self.layers = [torch.nn.Linear(in_features=self.n_nodes, out_features=self.n_nodes) for _ in range(self.n_layers)]
+        self.activation = torch.nn.ReLU()
+
+        # rng = np.random.default_rng()
+        # self.layers = [
+        #     prune.random_structured(
+        #         torch.nn.Linear(in_features=self.n_nodes, out_features=self.n_nodes,),
+        #         "weight",
+        #         amount=rng.random(1)[0]*0.5, dim=1) for _ in range(self.n_layers)
+        #     ]
+
+    def forward(self, x: torch.Tensor, *args, **kwargs):
+        for layer in self.layers:
+            x = layer(x)
+        return x
