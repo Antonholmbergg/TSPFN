e86c641ce60d782012a6a110f9ec4b9fcae66648
Anton Holmberg <aa@bazzite.fritz.box>
2025-09-12 10:15:09 +0200

    fixed dag test since I removed the get_graph method

--- tests/data/test_scm.py
+++ tests/data/test_scm.py
@@ -4,4 +4,4 @@ from tspfn.data.scm import get_scm
 
 
 def test_scm_is_dag():
-    assert nx.is_directed_acyclic_graph(get_scm(1).get_graph())
+    assert nx.is_directed_acyclic_graph(get_scm(1).graph)
