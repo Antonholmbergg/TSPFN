e69d5c6194e179357d75b1461fa1c9ad94d75f2f
Anton Holmberg <aa@bazzite.fritz.box>
2025-09-12 18:04:44 +0200

    first version of the nn edge mapping, still needs all of the non-linear mappings implemented in a better way

--- src/tspfn/data/edge_functions.py
+++ src/tspfn/data/edge_functions.py
@@ -1,10 +1,16 @@
 import logging
 from collections.abc import Callable
+from typing import Literal
 
 import numpy as np
 import torch
+from torch import nn
 
 logger = logging.getLogger()
+
+non_linearity_mapping = {
+    "relu": nn.ReLU(),
+}
 
 
 class EdgeFunctionSampler:
@@ -13,7 +19,7 @@ class EdgeFunctionSampler:
     ) -> None:
         logger.warning("This constructor is not actually iplermnted yet")
         self.function_prob = [0.3, 0.5, 0.2]
-        self.functions = [torch.nn.ReLU(), torch.nn.LeakyReLU(), torch.nn.Hardswish()]
+        self.functions = [nn.ReLU(), nn.LeakyReLU(), nn.Hardswish()]
         self.rng = np.random.default_rng(1)
 
     def sample(self) -> Callable:
@@ -24,8 +30,23 @@ def normalize():
     pass
 
 
+def small_nn(
+    latent_variable: torch.Tensor,
+    non_linearity: Literal["relu", "hardswish"],
+    torch_generator: torch.Generator | None = None,
+):
+    output_function = non_linearity_mapping[non_linearity]
+    _, columns = latent_variable.shape
+    w = torch.empty(columns, columns)
+    b = torch.empty(1, columns)
+    nn.init.xavier_normal_(w, generator=torch_generator)
+    nn.init.xavier_normal_(b, generator=torch_generator)
+    return output_function(latent_variable @ w.T + b)
+
+
 if __name__ == "__main__":
-    efs = EdgeFunctionSampler()
+    pass
+    # efs = EdgeFunctionSampler()
     # res = {0: 1, 1: 0, 2: 0}
     # for i in range(10_000):
 #        res[efs.sample()] += 1
