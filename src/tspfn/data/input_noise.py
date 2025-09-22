3f3cb48f71f23c429f4d7ee5a923456d1a269919
Anton Holmberg <anton.holmberg@live.com>
2025-09-22 21:06:22 +0200

    first scaffold of the noise sampling file

---
+++ src/tspfn/data/input_noise.py
@@ -0,0 +1,12 @@
+import torch
+from typing import Callable
+
+def generate_coloured_noise(nrows:int, n_cols:int, generator: torch.Generator, slope_max:float=3) -> torch.tensor:
+    pass
+
+def sample_function(generator: torch.Generator) -> Callable:
+    pass
+
+def get_input_noise(generator: torch.Generator):
+    #pass config as dict, object or seperate args? Depends on how many parameters I end up with 
+    pass
