97b6ad617b77c33add7b7fa3cebb373da7677225
Anton Holmberg <anton.holmberg@live.com>
2025-10-01 16:44:11 +0200

    sample prior working but sample function configs not finished

--- src/tspfn/data/prior.py
+++ src/tspfn/data/prior.py
@@ -1,8 +1,8 @@
-from typing import Self
-
+from typing import Self, Any
+import numpy as np
 import yaml
-from pydantic import BaseModel, ConfigDict
-
+from pydantic import BaseModel, ConfigDict, PrivateAttr
+from scipy.stats import loguniform, gamma, poisson
 from tspfn.data.utils import FunctionSampler, FunctionSamplingConfig, get_function
 
 
@@ -23,7 +23,7 @@ class Prior(BaseModel):
 class PriorConfig(BaseModel):
     seed: int
     n_datasets: int
-    n_node_lognorm_params: dict[str, float]
+    n_node_loguniform_params: dict[str, float]
     redirection_gamma_params: dict[str, float]
     feature_fraction_gamma_params: dict[str, float]
     node_dim_poisson_params: dict[str, float]
@@ -32,12 +32,17 @@ class PriorConfig(BaseModel):
     n_rows_poisson_params: dict[str, float]
     edge_function_configs: list[FunctionSamplingConfig]
     noise_function_configs: list[FunctionSamplingConfig]
+    _rng: np.random.RandomState = PrivateAttr()
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self._rng = np.random.RandomState(seed=self.seed)
 
     @classmethod
     def from_yaml_config(cls, file_path: str) -> Self:
         with open(file_path, encoding="utf-8") as f:
             config = yaml.safe_load(f)
-        # need to instansiate the registerd functions
+        # need to instansiate the registered functions
         for function_sampler_group in ["noise_function_configs", "edge_function_configs"]:
             sampler_group_config = config[function_sampler_group]
             for function_sampling_config in sampler_group_config:
@@ -45,13 +50,35 @@ class PriorConfig(BaseModel):
         return cls(**config)
 
     def sample_prior(self) -> Prior:
-        pass
+        n_nodes = int(loguniform.rvs(**self.n_node_loguniform_params, random_state=self._rng))
+        redirection_probability = gamma.rvs(**self.redirection_gamma_params, random_state=self._rng)
+        feature_fraction_gamma_params = gamma.rvs(**self.feature_fraction_gamma_params, random_state=self._rng)
+        n_sample_rows = poisson.rvs(**self.n_rows_poisson_params, random_state=self._rng)
+        node_dim = poisson.rvs(**self.node_dim_poisson_params, random_state=self._rng)
+        edge_function_sampler = self.__sample_function_configs(self.edge_function_configs)
+        input_noise_function_sampler = self.__sample_function_configs(self.noise_function_configs)
+        seed = self._rng.randint(0, 1_000_000_000)
+        return Prior(
+            n_nodes_total=n_nodes,
+            redirection_probablility=redirection_probability,
+            seed=seed,
+            feature_node_fraction=feature_fraction_gamma_params,
+            edge_function_sampler=edge_function_sampler,
+            input_noise_function_sampler=input_noise_function_sampler,
+            n_sample_rows=n_sample_rows,
+            node_dim=node_dim,
+            edge_noise_std=self.edge_noise_std,
+            n_draws_feature_mapping=self.n_draws_feature_mapping,
+        )
 
-    def __sample_function_configs(
-        self,
-    ) -> FunctionSampler:
-        pass
+    def __sample_function_configs(self, func_sampling_configs: list[FunctionSamplingConfig]) -> FunctionSampler:
+        """::TODO sample the weights of the config such that it is not exactly the same each time"""
+        return FunctionSampler(func_sampling_configs)
 
 
 if __name__ == "__main__":
     prior_config = PriorConfig.from_yaml_config("configs/data/prior_testing.yaml")
+    prior1 = prior_config.sample_prior()
+    print(prior1)
+    prior2 = prior_config.sample_prior()
+    print(prior2)
