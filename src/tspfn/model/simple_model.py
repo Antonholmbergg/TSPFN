a6cf97dd73338c5113ee92fe823ebfed0e000c09
Anton Holmberg <anton.holmberg@live.com>
2025-10-19 13:48:08 +0200

    more fleshed out model implementation

--- src/tspfn/model/simple_model.py
+++ src/tspfn/model/simple_model.py
@@ -1,4 +1,4 @@
-from typing import Self
+from collections.abc import Callable
 
 import lightning
 import torch
@@ -6,25 +6,50 @@ from torch import nn
 
 
 class SimpleModel(lightning.LightningModule):
-    def __init__(self, encoder):
+    def __init__(
+        self,
+        input_dim: int,
+        model_dim: int,
+        n_atention_heads: int,
+        n_encoder_layers: int,
+        output_dim: int = 1,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+        activation: str | Callable = "gelu",
+        *,
+        batch_first: bool = False,
+        norm_first: bool = False,
+        encoder_norm: None | nn.Module = None,
+        enable_nested_tensor: bool = True,
+        mask_check: bool = True,
+    ):
         super().__init__()
-        self.encoder = encoder
+        self.linear_embedding = nn.Linear(input_dim, model_dim)
+        encoder_layer = nn.TransformerEncoderLayer(
+            model_dim,
+            n_atention_heads,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+            activation=activation,
+            batch_first=batch_first,
+            norm_first=norm_first,
+        )
+        self.transformer = nn.TransformerEncoder(
+            encoder_layer,
+            n_encoder_layers,
+            norm=encoder_norm,
+            enable_nested_tensor=enable_nested_tensor,
+            mask_check=mask_check,
+        )
+        self.forecast_head = nn.Linear(model_dim, output_dim)
 
     def training_step(self, batch, batch_idx):
         x, _ = batch
         x = x.view(x.size(0), -1)
-        z = self.encoder(x)
+        x_hat = self.encoder(x)
         loss = nn.functional.mse_loss(x_hat, x)
         self.log("train_loss", loss)
         return loss
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
-
-
-class TimeSeriesPFN(nn.Module):
-    def __init__(self, d_model: int, nhead: int, num_layers: int, n_covariates: int) -> Self:
-        self.value_embedding = nn.Linear(1 + n_covariates, d_model)
-        self.time_encoding = 1
-        self.transformer = nn.TransformerEncoder(...)
-        self.forecast_head = nn.Linear(d_model, 1)
