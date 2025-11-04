991fc5366943b076fe63d23d79f4b8e66f35250d
Anton Holmberg <anton.holmberg@live.com>
2025-11-04 20:30:25 +0100

    implemented the simplest version of the mode l lightning module, not done

--- src/tspfn/model/simple_model.py
+++ src/tspfn/model/simple_model.py
@@ -12,10 +12,12 @@ class SimpleModel(lightning.LightningModule):
         model_dim: int,
         n_atention_heads: int,
         n_encoder_layers: int,
+        loss: Callable[[torch.Tensor], torch.Tensor],
         output_dim: int = 1,
         dim_feedforward: int = 2048,
         dropout: float = 0.1,
-        activation: str | Callable = "gelu",
+        activation: str | Callable[[torch.Tensor], torch.Tensor] = "gelu",
+        lr: float = 1e-3,
         *,
         batch_first: bool = False,
         norm_first: bool = False,
@@ -24,7 +26,8 @@ class SimpleModel(lightning.LightningModule):
         mask_check: bool = True,
     ):
         super().__init__()
-        self.linear_embedding = nn.Linear(input_dim, model_dim)
+        self.save_hyperparameters()
+        self.linear_embedding_layer: nn.Module = nn.Linear(input_dim, model_dim)
         encoder_layer = nn.TransformerEncoderLayer(
             model_dim,
             n_atention_heads,
@@ -34,24 +37,31 @@ class SimpleModel(lightning.LightningModule):
             batch_first=batch_first,
             norm_first=norm_first,
         )
-        self.transformer = nn.TransformerEncoder(
+        self.transformer: nn.Module = nn.TransformerEncoder(
             encoder_layer,
             n_encoder_layers,
             norm=encoder_norm,
             enable_nested_tensor=enable_nested_tensor,
             mask_check=mask_check,
         )
-        self.forecast_head = nn.Linear(model_dim, output_dim)
+        self.forecast_head: nn.Module = nn.Linear(model_dim, output_dim)
+        self.loss = loss
+        self.lr = lr
 
-    def training_step(self, batch, batch_idx):
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        x_embedded = self.linear_embedding_layer(inputs)
+        x_latent = self.transformer(x_embedded)
+        return self.forecast_head(x_latent)
+
+    def training_step(self, batch: tuple[torch.Tensor, torch.Tensor], *args):
         x, y = batch
-        x_hat = self.transformer(x)
-        loss = nn.functional.mse_loss(x_hat, x)
+        y_hat = self(x, y)
+        loss = self.loss(y_hat, y)
         self.log("train_loss", loss)
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
 
 
 if __name__ == "__main__":
