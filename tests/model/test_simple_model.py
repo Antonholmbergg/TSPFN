044b16eb7fc4a44fb7bb2fc2d2e0876c669ed84c
Anton Holmberg <anton.holmberg@live.com>
2025-11-05 20:39:28 +0100

    added a test that checks if the simple model runs through (not necessarily correctly) with normal tensors and nested jagged tensors

---
+++ tests/model/test_simple_model.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+
+from tspfn.model.simple_model import SimpleModel
+
+
+def test_runs_with_nested_jagged_tensors():
+    model = SimpleModel(
+        input_dim=64,
+        model_dim=64,
+        n_attention_heads=8,
+        n_transformer_blocks=4,
+        loss=nn.MSELoss(),
+        dim_feedforward=512,
+        output_dim=64,
+        is_causal=True,
+    )
+    model.configure_model()
+    inp = torch.nested.nested_tensor(
+        tensor_list=[torch.rand(100, 64), torch.rand(10, 64), torch.rand(50, 64)], layout=torch.jagged
+    )
+    model(inp)
+
+
+def test_runs_with_normal_tensors():
+    model = SimpleModel(
+        input_dim=64,
+        model_dim=64,
+        n_attention_heads=8,
+        n_transformer_blocks=4,
+        loss=nn.MSELoss(),
+        dim_feedforward=512,
+        output_dim=64,
+        is_causal=True,
+    )
+    model.configure_model()
+    inp = torch.rand(2, 100, 64)
+    model(inp)
